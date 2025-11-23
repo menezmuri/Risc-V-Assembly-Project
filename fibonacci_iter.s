cb39de56f2c83fe071954c7ad3234e74426fcca3
Murilo <menez.muri@gmail.com>
2025-11-22 21:34:58 -0300

    Implement overflow detection in Fibonacci iterative version and remove recursive version

--- fibonacci_iter.s
+++ fibonacci_iter.s
@@ -1,7 +1,8 @@
 .data
-prompt: .asciiz "n = "
+prompt: .asciz "n = "
+overflow_msg: .asciz "overflow: resultado excede 32-bit\n"
 .text
-.global main
+.globl main
 main:
     la a0, prompt
     li a7, 4
@@ -13,12 +14,17 @@ main:
 
     li t1, 0        # a = 0
     li t2, 1        # b = 1
+    li t6, 2147483647  # MAX signed 32-bit for overflow detection
     beqz t0, fib_print_zero
 
     li t3, 1        # i = 1
 loop_check:
     slt t4, t3, t0  # t4 = (i < n)
     beqz t4, fib_print_result
+    # Overflow check: if t1 > MAX - t2 then adding will overflow signed 32-bit
+    sub t5, t6, t2   # t5 = MAX - t2  (use t5 as temp)
+    slt t4, t5, t1   # t4 = (t5 < t1) ? 1 : 0  => overflow if 1
+    bnez t4, fib_overflow
     add t5, t1, t2
     mv t1, t2
     mv t2, t5
@@ -39,3 +45,9 @@ fib_print_result:
 fib_exit:
     li a7, 10
     ecall
+
+fib_overflow:
+    la a0, overflow_msg
+    li a7, 4
+    ecall
+    j fib_exit
